7fdc838ef45897b1f96df05ef5bb415271359fdf
Swarup Waghade <Swarup Waghade@DESKTOP-9R71SBT>
2021-03-30 16:45:36 +0530

    Initial Commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _Context;
+
+        public ProductsController(StoreContext context)
+        {
+            _Context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IList<Product>>> GetProduct()
+        {
+            var products = await _Context.Products.ToListAsync();
+
+            if (products != null)
+            {
+                return Ok(products);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await _Context.Products.FindAsync(id);
+
+            if (product != null)
+            {
+                return Ok(product);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ Infrastructure/StoreContext.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
