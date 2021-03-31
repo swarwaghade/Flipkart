95ec9466ec241dbf32fa70c7119f11c96f308e68
Swarup Waghade <Swarup Waghade@DESKTOP-9R71SBT>
2021-03-31 19:08:33 +0530

    GenericsBeforeDtos

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using Core.Entities;
 using Core.Interfaces;
+using Core.Specifications;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,17 +12,23 @@ namespace API.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
-        private readonly IProductRepository _Repo;
+        private readonly IGenericRepository<Product> _repoProduct;
+        private readonly IGenericRepository<ProductBrand> _repoProductBrand;
+        private readonly IGenericRepository<ProductType> _repoProductType;
 
-        public ProductsController(IProductRepository repo)
+        public ProductsController(IGenericRepository<Product> repoProduct
+        ,IGenericRepository<ProductBrand> repoProductBrand,IGenericRepository<ProductType> repoProductType)
         {
-            _Repo = repo;
+            _repoProduct = repoProduct;
+            _repoProductBrand = repoProductBrand;
+            _repoProductType = repoProductType;
         }
 
         [HttpGet]
         public async Task<ActionResult<IList<Product>>> GetProduct()
         {
-            var products = await _Repo.GetProductsAsync();
+            var spec = new ProductsWithTypeAndBrandSpecificaion();
+            var products = await _repoProduct.ListAsync(spec);
 
             if (products != null)
             {
@@ -37,7 +44,7 @@ namespace API.Controllers
         [HttpGet("types")]
         public async Task<ActionResult<IList<ProductType>>> GetProductTypes()
         {
-            var productTypes = await _Repo.GetProductTypeAsync();
+            var productTypes = await _repoProductType.ListAllAsync();
 
             if (productTypes != null)
             {
@@ -52,7 +59,7 @@ namespace API.Controllers
         [HttpGet("brands")]
         public async Task<ActionResult<IList<ProductBrand>>> GetProductBrands()
         {
-            var productBrand = await _Repo.GetProductBrandAsync();
+            var productBrand = await _repoProductBrand.ListAllAsync();
 
             if (productBrand != null)
             {
@@ -67,7 +74,8 @@ namespace API.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            var product = await _Repo.GetProductByIdAsync(id);
+             var spec = new ProductsWithTypeAndBrandSpecificaion(id);
+            var product = await _repoProduct.GetEntityWithSpec(spec);
 
             if (product != null)
             {
